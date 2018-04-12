39096c8b279aa963784afe76efb4b9ba0b5d1373
David H. Wei <davidwei@dunelm.org.uk>
2018-04-12 10:11:44 +0100

    Restore accidentally deleted Link classes

---
+++ jmock/src/main/java/org/jmock/internal/perf/link/ProbabilisticBranch.java
@@ -0,0 +1,26 @@
+package org.jmock.internal.perf.network.link;
+
+import org.jmock.internal.perf.network.Network;
+import org.jmock.internal.perf.network.node.Node;
+import org.jmock.internal.perf.network.request.Customer;
+
+public class ProbabilisticBranch<T extends Customer> extends Link<T> {
+    private DiscreteSampler dist;
+    private Node[] nodes;
+
+    public ProbabilisticBranch(Network network, double[] probs, Node[] nodes) {
+        super(network);
+        this.dist = new DiscreteSampler(probs);
+        this.nodes = nodes;
+    }
+
+    public void move(T customer) {
+        int next = dist.next();
+        if (next == 0) {
+            System.out.println("\nProbabilisticBranch: sending thread " + customer.threadId() + " to Sink!\n");
+        } else {
+            System.out.println("ProbabilisticBranch: sending thread " + customer.threadId() + " to " + nodes[next].name());
+        }
+        send(customer, nodes[next]);
+    }
+}
